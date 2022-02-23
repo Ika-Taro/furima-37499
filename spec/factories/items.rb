cbe86b22c2e628039e1633b66edfc6c96f2e30b9
Ika-Taro <black.jkh@gmail.com>
2022-02-23 16:32:43 +0900

    販売価格計算の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image
+    item_name
+    item_description
+    item_category_id
+    item_condition_id
+    delivery_charge_id
+    prefecture_id
+    take_for_shipping_id
+    selling_price
   end
 end
