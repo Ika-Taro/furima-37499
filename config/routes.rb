bc3ef6b854d8a93095f02488969b7f661e2d17b5
Ika-Taro <black.jkh@gmail.com>
2022-02-23 20:52:37 +0900

    商品一覧機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get 'items/index'
+  # get 'items/index'
   root to: "items#index"
 
-  resources :items , only: [:index, :create, :new]
+  resources :items , only: [:index, :create, :new, :show]
 end
