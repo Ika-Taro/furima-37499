a02e3a4ffda6b48d1abe574e2c8f893ac20cc786
Ika-Taro <black.jkh@gmail.com>
2022-02-28 17:06:23 +0900

    データベース保存まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,27 @@ class OrdersController < ApplicationController
   # before_action :authenticate_user!
   
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
 
+
+  def create
+    # binding.pry
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(order_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id).merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,26 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, prefecture_id, municipality, adress, building_name, phone_number, phurchase_record_id, user_id, item_id
+  attr_accessor :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :phurchase_record_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :post_code
+    validates :municipality
+    validates :address
+    validates :phone_number
+    validates :user_id
+    validates :item_id
+  
+  end
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    # validates :building_name
+
+  def save
+    # 寄付情報を保存し、変数donationに代入する
+    purchaserecord = PurchaseRecord.create(item_id: item_id, user_id: user_id)
+    # 住所を保存する
+
+
+    # donation_idには、変数donationのidと指定する
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number,purchase_record_id: purchaserecord.id)
+  end
 end
