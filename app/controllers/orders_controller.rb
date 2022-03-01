0168618e6b5c2df5d1c674fac5405f7ca8666e20
Ika-Taro <black.jkh@gmail.com>
2022-03-01 15:30:51 +0900

    商品購入機能の実装　微調整2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,9 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
+    if @item.purchase_record.present?
+      redirect_to root_path
+    end
   end
 
 
